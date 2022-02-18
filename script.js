2d02f693f858f821d107c86e6cd8e0f3b41967e5
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-02-18 11:23:58 -0300

    refatoração e alguns ajustes

--- script.js
+++ script.js
@@ -2,14 +2,7 @@
 
 const landingButton = document.getElementById('landing__button')
 
-function handleScroll(top) {
-  window.scrollTo({
-    top: top - 35,
-    behavior: 'smooth'
-  })
-}
-
-landingButton.addEventListener('click', () => handleScroll(top = headingTopPositions[0]));
+landingButton.addEventListener('click', () => handleScroll(headingTopPositions[0]));
 
 
 // a tabela de conteúdos deve aparecer apenas quando o scroll estiver no primeiro parágrafo
@@ -21,13 +14,21 @@ const outlineContainer = document.querySelector('.outline__list-container')
 let scrollPosition = 0
 
 document.addEventListener('scroll', (e) => {
+  const media = '(min-width: 1200px) and (max-width: 1800px) and (orientation: landscape)'
+  const matchMedia = window.matchMedia(media).matches
+  const main = document.querySelector('.main')
+  
   scrollPosition = window.scrollY
+  
   if (scrollPosition >= headingTopPositions[0] - 100) {
     outline.style.left = '20px'
     outlineContainer.style.transform = 'translateX(10px)'
+    matchMedia ? main.style.marginLeft = '15%' : main.style.marginLeft = '0px'
   } else {
     outline.style.left = '-300px'
     outlineContainer.style.transform = 'translateX(-50px)'
+
+    main.style.marginLeft = '0'
   }
 })
 
@@ -50,6 +51,15 @@ outlineList.forEach((item) => {
 
   item.addEventListener('click', () => {
     const index = item.dataset.index
-    handleScroll(top = headingTopPositions[index])
+    handleScroll(headingTopPositions[index])
   })
 })
+
+// função para fazer o scroll
+
+function handleScroll(top) {
+  window.scrollTo({
+    top: top - 35,
+    behavior: 'smooth'
+  })
+}
